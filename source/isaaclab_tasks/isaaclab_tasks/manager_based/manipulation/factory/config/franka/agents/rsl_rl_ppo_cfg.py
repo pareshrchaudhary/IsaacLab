86cf8e737bb631e4ac719b8538a5324e9dbff6a5
Octi Zhang <zhengyuz@nvidia.com>
2025-06-30 21:49:02 -0700

    normalization default to True

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/factory/config/franka/agents/rsl_rl_ppo_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/factory/config/franka/agents/rsl_rl_ppo_cfg.py
@@ -14,7 +14,7 @@ class FactoryPPORunnerCfg(RslRlOnPolicyRunnerCfg):
     max_iterations = 1500
     save_interval = 50
     experiment_name = "factory"
-    empirical_normalization = False
+    empirical_normalization = True
     policy = RslRlPpoActorCriticCfg(
         init_noise_std=1.0,
         actor_hidden_dims=[512, 128, 64],
